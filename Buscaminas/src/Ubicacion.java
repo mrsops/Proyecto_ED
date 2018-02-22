42050f3db2474998ee2d7136c26ba9b70decbf2b
Sopss <36036092+mrsops@users.noreply.github.com>
2018-02-22 09:58:14 +0100

    Primer push de la interfaz mas o menos

---
+++ Buscaminas/src/Ubicacion.java
@@ -0,0 +1,12 @@
+public class Ubicacion {
+
+    private int ejeY;
+    private int ejeX;
+
+    public Ubicacion(int ejeY, int ejeX) {
+        this.ejeY = ejeY;
+        this.ejeX = ejeX;
+    }
+
+
+}
